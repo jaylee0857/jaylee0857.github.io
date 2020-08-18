23e78449524e04fa7f8393f87951338d64b66869
jaylee0857 <58556192+jaylee0857@users.noreply.github.com>
2020-08-18 10:23:08 +0800

    up

--- capoo/js/index.js
+++ capoo/js/index.js
@@ -1,3 +1,28 @@
-$(".active").click(function () {
-    $(".hamburger ul").toggle();
-})
+(function () {
+    AOS.init();
+
+    $(".active").click(function () {
+        $(".hamburger ul").toggle();
+    })
+
+    $(".mobile ul li").click(function (){
+        $(".hamburger ul").toggle();
+    })
+
+    var swiper = new Swiper('.swiper-container', {
+        loop: true,
+        effect: 'cube',
+        grabCursor: true,
+        cubeEffect: {
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.94,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+
+})()
+
